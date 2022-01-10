b8d0f64f3e3de8cf567319ef7148c4efa5080493
RichardDan2k1 <ricdan2k1@gmail.com>
2022-01-10 10:31:38 +0530

    custom encrypted chat app
    final commit

---
+++ Chat_Client_App/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace Chat_Client_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.textLocalIp = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFriendsPort = new System.Windows.Forms.TextBox();
+            this.textFriendsIp = new System.Windows.Forms.TextBox();
+            this.ChatWindow = new System.Windows.Forms.ListBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textLocalPort);
+            this.groupBox1.Controls.Add(this.textLocalIp);
+            this.groupBox1.Location = new System.Drawing.Point(62, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 125);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User One";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PORT NO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP ADDR";
+            // 
+            // textLocalPort
+            // 
+            this.textLocalPort.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textLocalPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textLocalPort.Location = new System.Drawing.Point(88, 83);
+            this.textLocalPort.Name = "textLocalPort";
+            this.textLocalPort.Size = new System.Drawing.Size(162, 27);
+            this.textLocalPort.TabIndex = 2;
+            // 
+            // textLocalIp
+            // 
+            this.textLocalIp.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textLocalIp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textLocalIp.Location = new System.Drawing.Point(88, 40);
+            this.textLocalIp.Name = "textLocalIp";
+            this.textLocalIp.Size = new System.Drawing.Size(162, 27);
+            this.textLocalIp.TabIndex = 0;
+            this.textLocalIp.TextChanged += new System.EventHandler(this.textLocalIp_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textFriendsPort);
+            this.groupBox2.Controls.Add(this.textFriendsIp);
+            this.groupBox2.Location = new System.Drawing.Point(407, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 125);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "User Two";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PORT NO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "IP ADDR";
+            // 
+            // textFriendsPort
+            // 
+            this.textFriendsPort.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textFriendsPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textFriendsPort.Location = new System.Drawing.Point(85, 83);
+            this.textFriendsPort.Name = "textFriendsPort";
+            this.textFriendsPort.Size = new System.Drawing.Size(165, 27);
+            this.textFriendsPort.TabIndex = 1;
+            // 
+            // textFriendsIp
+            // 
+            this.textFriendsIp.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textFriendsIp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textFriendsIp.Location = new System.Drawing.Point(85, 39);
+            this.textFriendsIp.Name = "textFriendsIp";
+            this.textFriendsIp.Size = new System.Drawing.Size(165, 27);
+            this.textFriendsIp.TabIndex = 0;
+            // 
+            // ChatWindow
+            // 
+            this.ChatWindow.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ChatWindow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ChatWindow.FormattingEnabled = true;
+            this.ChatWindow.ItemHeight = 20;
+            this.ChatWindow.Location = new System.Drawing.Point(62, 214);
+            this.ChatWindow.Name = "ChatWindow";
+            this.ChatWindow.Size = new System.Drawing.Size(595, 142);
+            this.ChatWindow.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Location = new System.Drawing.Point(62, 381);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(482, 27);
+            this.textBox5.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(681, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(563, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "SEND";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Snow;
+            this.label5.Location = new System.Drawing.Point(281, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(203, 50);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Chat App";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(208, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.ChatWindow);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.TextBox textLocalIp;
+        private System.Windows.Forms.TextBox textFriendsPort;
+        private System.Windows.Forms.TextBox textFriendsIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox ChatWindow;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Chat_Client_App/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat_Client_App
+{
+    public partial class Form1 : Form
+    {
+        Socket sck;
+        EndPoint epLocal, epRemote;
+        public Form1()
+        {
+            InitializeComponent();
+
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            textLocalIp.Text = GetLocalIP();
+            textFriendsIp.Text = GetLocalIP();
+        }
+
+        //function to get the local ip
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            
+            foreach(IPAddress ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return "127.0.0.1";
+        }
+
+        //decryption function
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                int size = sck.EndReceiveFrom(aResult, ref epRemote);
+
+                if (size > 0)
+                {
+                    byte[] receivedData = new byte[1464];
+
+                    receivedData = (byte[])aResult.AsyncState;
+
+                    //converting byte array to string
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string receivedMessage = eEncoding.GetString(receivedData);
+
+                    //add new decrypt function here should return string
+                    string customMessage = customDecryption(receivedMessage);
+
+                    //decrypt-caesar
+                    string rMsg = Decipher(customMessage, 3);
+
+                    //idea : could add a random vairable as the key
+                    //helps in getting real end to end encryption
+                    //session wise and could expire with timer. also new random variable for every message.
+                    ChatWindow.Items.Add("Friend : " + rMsg);
+                }
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        //connection function
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                epLocal = new IPEndPoint(IPAddress.Parse(textLocalIp.Text), Convert.ToInt32(textLocalPort.Text));
+                sck.Bind(epLocal);
+
+                //connecting local IP with remote IP
+                epRemote = new IPEndPoint(IPAddress.Parse(textFriendsIp.Text), Convert.ToInt32(textFriendsPort.Text));
+                sck.Connect(epRemote);
+
+                //start listening now
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+
+                button1.Text = "Connected";
+                button1.Enabled = false;
+                button2.Enabled = true;
+                textBox5.Focus();
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+        }
+
+        //encryption function
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+                msg = enc.GetBytes(textBox5.Text);
+
+                ASCIIEncoding eEncoding = new ASCIIEncoding();
+                string dMsg = eEncoding.GetString(msg);
+
+                //encryption-caesar
+                string sMsg = Encipher(dMsg, 3);
+
+                //could add a layer of encryption here
+                string customMessage = customEncryption(sMsg);
+
+                //convert to byte array to send through TCP
+                byte[] enc_msg = eEncoding.GetBytes(customMessage);
+
+                sck.Send(enc_msg);
+
+                ChatWindow.Items.Add("You : " + textBox5.Text);
+                textBox5.Clear();
+            }
+            catch(Exception ex1)
+            {
+                MessageBox.Show(ex1.ToString());
+            }
+        }
+
+        private static char Cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - offset) % 26) + offset);
+        }
+
+        public static string Encipher(string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += Cipher(ch, key);
+
+            return output;
+        }
+
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 26 - key);
+        }
+
+        // Custom Encryption STARTS ////////////////////////////
+
+        public static string customEncryption(string ss)
+        {
+            char[] s = ss.ToCharArray();
+            int l = s.Length;
+            int b = (int)Math.Ceiling(Math.Sqrt(l));
+            int a = (int)Math.Floor(Math.Sqrt(l));
+            string encrypted = string.Empty;
+            if (b * a < l)
+            {
+                if (Math.Min(b, a) == b)
+                {
+                    b = b + 1;
+                }
+                else
+                {
+                    a = a + 1;
+                }
+            }
+            char[,] arr = new char[a, b];
+            int k = 0;
+
+            for (int j = 0; j < a; j++)
+            {
+                for (int i = 0; i < b; i++)
+                {
+                    if (k < l)
+                    {
+                        arr[j, i] = s[k];
+                    }
+                    k++;
+                }
+            }
+
+            // Loop to generate
+            // encrypted String
+            for (int j = 0; j < b; j++)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    encrypted = encrypted +
+                                arr[i, j];
+                }
+            }
+            return encrypted;
+        }
+
+
+        public static string customDecryption(string ss)
+        {
+            char[] s = ss.ToCharArray();
+            int l = s.Length;
+            int b = (int)Math.Ceiling(Math.Sqrt(l));
+            int a = (int)Math.Floor(Math.Sqrt(l));
+            string decrypted = string.Empty;
+
+            // Matrix to generate the
+            // Encrypted String
+            char[,] arr = new char[a, b];
+            int k = 0;
+
+            // Fill the matrix column-wise
+            for (int j = 0; j < b; j++)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    if (k < l)
+                    {
+                        arr[j, i] = s[k];
+                    }
+                    k++;
+                }
+            }
+
+            // Loop to generate
+            // decrypted String
+            for (int j = 0; j < a; j++)
+            {
+                for (int i = 0; i < b; i++)
+                {
+                    decrypted = decrypted +arr[i, j];
+                }
+            }
+            return decrypted;
+        }
+
+        // Custom Encryption ENDS ////////////////////////////
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textLocalIp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
